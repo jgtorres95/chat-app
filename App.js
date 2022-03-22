ca297c477804110dd6a8e5a43ba5331e4afc2a6c
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-03-21 22:30:35 -0700

    create Navigator

--- App.js
+++ App.js
@@ -10,6 +10,28 @@ import Chat from './components/Chat';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// Create the navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Start"
+        >
+          <Stack.Screen
+            name="Start"
+            component={Start}
+          />
+          <Stack.Screen
+            name="Chat"
+            component={Chat}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
