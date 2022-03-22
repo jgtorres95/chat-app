3457920a9dea7af04d98173494b306c5f5ca4ad4
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-03-21 22:30:20 -0700

    add import statements

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Import Start and Chat screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+
+//import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
 }
 
 const styles = StyleSheet.create({
