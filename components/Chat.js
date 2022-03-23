557cd33b5f1917bc5d65234e33c015c354ee17fa
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-03-23 00:44:09 -0700

    create componentDidMount

--- components/Chat.js
+++ components/Chat.js
@@ -14,6 +14,29 @@ export default class Chat extends React.Component {
     }
   }
 
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+         },
+         {
+          _id: 2,
+          text: `${this.props.route.params.name} has entered the chat`,
+          createdAt: new Date(),
+          system: true,
+         },
+      ]
+    })
+  }
+
   render() {
 
     let name = this.props.route.params.name;
