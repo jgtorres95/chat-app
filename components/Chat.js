6b268701f81c0b6a4957953f9d3675d310b6b4df
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-04-03 12:15:14 -0700

    create renderCustomView method

--- components/Chat.js
+++ components/Chat.js
@@ -220,6 +220,28 @@ export default class Chat extends React.Component {
     return <CustomActions {...props} />
   };
 
+  renderCustomView (props) {
+    const { currentMessage} = props;
+    if (currentMessage.location) {
+      return (
+          <MapView
+            style={{width: 150,
+              height: 100,
+              borderRadius: 13,
+              margin: 3}}
+            region={{
+              latitude: currentMessage.location.latitude,
+              longitude: currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          />
+      );
+    }
+    return null;
+  }
+ 
+
   render() {
 
     let backgroundColor = this.props.route.params.backgroundColor;
